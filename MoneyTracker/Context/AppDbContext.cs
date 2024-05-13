b13408c5f0f844a53e660fc2cc3327605cd128a6
RMLemos <143395332+RMLemos@users.noreply.github.com>
2024-05-13 16:01:24 +0100

    Add project files.

---
+++ MoneyTracker/Context/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoneyTracker.Models;
+
+namespace MoneyTracker.Context;
+
+public class AppDbContext : DbContext 
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {}
+
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Expense> Expenses { get; set; }
+}
---
+++ MoneyTracker/Models/Expense.cs
@@ -0,0 +1,12 @@
+﻿namespace MoneyTracker.Models;
+
+public class Expense
+{
+    public int ExpenseId { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+    public string? Payment { get; set; }
+    public int CategoryId { get; set; }
+    public virtual Category? Category { get; set; }
+}
